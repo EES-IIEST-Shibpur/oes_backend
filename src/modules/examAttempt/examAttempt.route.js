17d754a1671a0843b74bc7544fb3e2d533860557
Aminul Islam <aminulislam03905@gmail.com>
2026-01-03 18:17:19 +0530

    [FIX]: routes are become protected

--- src/modules/examAttempt/examAttempt.route.js
+++ src/modules/examAttempt/examAttempt.route.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { getExamForAttempt, saveAnswer, startExam, submitExam } from "./examAttempt.controller";
+import { requireAuth, requireEmailVerified } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
+
+router.use(requireAuth);
+router.use(requireEmailVerified);
 
 router.get("/:examId/start", startExam);
 router.get("/:examId/attempt", getExamForAttempt);
--- src/modules/result/result.router.js
+++ src/modules/result/result.router.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { getMyResult } from "./result.controller";
+import { requireAuth, requireEmailVerified } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
+
+router.use(requireAuth);
+router.use(requireEmailVerified);
 
 router.get("/:examId", getMyResult);
 
